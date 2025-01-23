9480dceb98b0c5096b733dc486b3ebab558bae76
GitforJosh375 <joshualilly14@gmail.com>
2025-01-23 16:57:45 -0600

    fixed error

--- client/src/routes/login.jsx
+++ client/src/routes/login.jsx
@@ -1,8 +1,7 @@
 import Navbar from "../components/Navbar.jsx";
 import Hero1 from "../components/Hero1.jsx";
-import LoginPage from "../assets/LoginPage.jpg"
 import Footer from "../components/Footer.jsx"
-import LoginMe from "../components/LoginMe.jsx"
+import LoginMe from "../components/loginMe.jsx"
 
 
 function Login () {
